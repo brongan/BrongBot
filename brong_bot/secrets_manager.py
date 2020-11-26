c00999a9b59359087c174daadc95eee8aa7060dc
Brennan Tracy <brennantracy4@gmail.com>
2020-11-25 19:53:34 -0800

    Remove unnecessary error handling

--- brong_bot/secrets_manager.py
+++ brong_bot/secrets_manager.py
@@ -1,47 +1,25 @@
 import base64
+import logging
 
 import boto3
 from botocore.exceptions import ClientError
 
 
-def get_discord_api_key():
-    secret_name = "BrongBot/DiscordToken"
-    region_name = "us-west-2"
+SECRET_NAME = "BrongBot/DiscordToken"
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+def get_discord_api_key():
+    client = boto3.client('secretsmanager')
 
     try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        response = client.get_secret_value(SecretId=SECRET_NAME)
     except ClientError as e:
-        if e.response["Error"]["Code"] == "DecryptionFailureException":
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "InternalServiceErrorException":
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "InvalidParameterException":
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "InvalidRequestException":
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "ResourceNotFoundException":
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+        error_code = e.response['Error']['Code']
+        logging.error("Failed to get discord api_key %s", error_code)
     else:
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if "SecretString" in get_secret_value_response:
-            secret = get_secret_value_response["SecretString"]
+        if "SecretString" in response:
+            return response["SecretString"]
         else:
-            decoded_binary_secret = base64.b64decode(
-                get_secret_value_response["SecretBinary"]
+            return base64.b64decode(
+                response["SecretBinary"]
             )
-    return secret
